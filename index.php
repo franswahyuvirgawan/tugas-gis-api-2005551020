cd7b3faf40267645062b4f05d4c7ebf33374d59f
Frans701 <franswahyu7@gmail.com>
2023-10-09 16:51:28 +0800

    revision api

--- index.php
+++ index.php
@@ -1,103 +1,111 @@
 <?php
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
-header("Access-Control-Allow-Origin: *");
+ini_set('display_errors',1);
+header("Access-Control-Allow-Origin:* ");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
 
-include 'DBConnect.php'; // Pastikan file DBConnect.php ada dan sudah benar
-$objDB = new DBConnect;
-$conn = $objDB->connect();
+$db_conn= mysqli_connect("localhost","root", "root", "react-crud");
+if($db_conn===false)
+{
+  die("ERROR: Could Not Connect".mysqli_connect_error());
+}
 
 $method = $_SERVER['REQUEST_METHOD'];
-switch ($method) {
-    case 'GET':
-        $sql = "SELECT * FROM gis";
-        $path = explode('/', $_SERVER['REQUEST_URI']);
-        if (isset($path[3]) && is_numeric($path[3])) {
-            $sql = "SELECT * FROM gis WHERE id = :id";
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':id', $path[3], PDO::PARAM_INT); // Tambahkan tipe data PDO::PARAM_INT
-            $stmt->execute();
-            $gis = $stmt->fetch(PDO::FETCH_ASSOC);
-            if ($gis) {
-                echo json_encode($gis);
-            } else {
-                echo json_encode(['error' => 'Data not found']);
-            }
-        } else {
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
-            $gis = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            echo json_encode($gis);
-        }
+//echo "test----".$method; die;
+switch($method)
+{
+  case "GET": 
+    $path= explode('/', $_SERVER['REQUEST_URI']);
+
+    if(isset($path[4]) && is_numeric($path[4]))
+    {
+      $json_array= array();
+      $id= $path[4];
+      
+      $getgisrow= mysqli_query($db_conn, "SELECT * FROM gis WHERE id='$id' ");
+      while($gisrow= mysqli_fetch_array($getgisrow))
+      {
+       $json_array['rowgisdata']= array('id'=>$gisrow['id'],'lat'=>$gisrow['lat'], 'lng'=>$gisrow['lng'], 'locationName'=>$gisrow['locationName'],);
+      }
+      echo json_encode($json_array['rowgisdata']);
+      return;
+
+    } else { 
+
+    $allgis= mysqli_query($db_conn, "SELECT * FROM gis"); 
+    if(mysqli_num_rows($allgis) > 0)
+    {
+      while($row= mysqli_fetch_array($allgis))
+      {
+       $json_array["gisdata"][]= array("id"=>$row['id'], "lat"=>$row["lat"], "lng"=>$row["lng"], "locationName"=>$row["locationName"]);
+      }
+      echo json_encode($json_array["gisdata"]);
+      return;
+    } else {
+        echo json_encode([]); 
+        return;
+    }
+  }   
+    break;
+    case "POST":
+      $gispostdata= json_decode(file_get_contents("php://input"));
+      //echo "sucess data";
+      //print_r($gispostdata); die;
+      $lat= $gispostdata->lat;
+      $lng= $gispostdata->lng;
+      $locationName= $gispostdata->locationName;
+      $result= mysqli_query($db_conn, "INSERT INTO gis (lat, lng, locationName) 
+      VALUES('$lat', '$lng', '$locationName')");
+
+      if($result)
+      {
+        echo json_encode(["success"=>"gis Added Successfully"]);
+        return;
+      } else {
+          echo json_encode(["success"=>"Please Check the gis Data!"]);
+          return; 
+      }
+      break;
+      case "PUT":
+        $gisUpdate= json_decode(file_get_contents("php://input"));
+
+         $id= $gisUpdate->id;
+         $lat= $gisUpdate->lat;
+         $lng= $gisUpdate->lng;
+         $locationName= $gisUpdate->locationName;
+
+         $updateData= mysqli_query($db_conn, "UPDATE gis SET lat='$lat', lng='$lng', locationName='$locationName' WHERE id='$id'  ");
+         if($updateData)
+         {
+           echo json_encode(["success"=>"gis Record Update Successfully"]);
+           return;
+         } else {
+             echo json_encode(["success"=>"Please Check the gis Data!"]);
+             return; 
+         }
+       // print_r($gisUpdate); die;
         break;
-    case 'POST':
-        $gis = json_decode(file_get_contents('php://input'));
-        $sql = "INSERT INTO gis(lat, lng, locationName, created_at) VALUES(:lat, :lng, :locationName, :created_at)"; // Hapus id, karena biasanya AUTO_INCREMENT
-        $stmt = $conn->prepare($sql);
-        $created_at = date('Y-m-d');
-        $stmt->bindParam(':lat', $gis->lat);
-        $stmt->bindParam(':lng', $gis->lng);
-        $stmt->bindParam(':locationName', $gis->locationName);
-        $stmt->bindParam(':created_at', $created_at);
-        if ($stmt->execute()) {
-            $response = ['status' => 1, 'message' => 'Record created successfully.'];
-        } else {
-            $response = ['status' => 0, 'message' => 'Failed to create record.'];
-        }
-        echo json_encode($response);
-        break;
-    case 'PUT':
-        $path = explode('/', $_SERVER['REQUEST_URI']);
-        $gis = json_decode(file_get_contents('php://input'));
-        $sql = "UPDATE gis SET lat = :lat, lng = :lng, locationName = :locationName, updated_at = :updated_at WHERE id = :id";
-        $stmt = $conn->prepare($sql);
-        $updated_at = date('Y-m-d');
-        $stmt->bindParam(':id', $path[3], PDO::PARAM_INT);
-        $stmt->bindParam(':lat', $gis->lat);
-        $stmt->bindParam(':lng', $gis->lng);
-        $stmt->bindParam(':locationName', $gis->locationName);
-        $stmt->bindParam(':updated_at', $updated_at);
+        case "DELETE":
+          // Baca data JSON dari body permintaan
+          $data = json_decode(file_get_contents("php://input"));
+          $locationId = $data->locationId;
         
-        if ($stmt->execute()) {
-            $response = ['status' => 1, 'message' => 'Record updated successfully.'];
-    
-            // Jika perubahan berhasil, Anda dapat mengambil data terbaru dari database
-            $sql = "SELECT * FROM gis WHERE id = :id";
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':id', $path[3], PDO::PARAM_INT);
-            if ($stmt->execute()) {
-                $updatedRecord = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-                // Sertakan data terbaru dalam respons
-                $response['data'] = $updatedRecord;
+          // Periksa apakah locationId ada dan apakah Anda ingin menjalankan DELETE
+          if (!empty($locationId)) {
+            $result = mysqli_query($db_conn, "DELETE FROM gis WHERE id = '$locationId'");
+            if ($result) {
+              echo json_encode(["success" => "gis Record Deleted Successfully"]);
+              return;
             } else {
-                $response = ['status' => 0, 'message' => 'Failed to fetch updated record.'];
+              echo json_encode(["error" => "Failed to delete gis record"]);
+              return;
             }
-        } else {
-            $response = ['status' => 0, 'message' => 'Failed to update record.'];
-        }
-        echo json_encode($response);
-        break;
-    case 'DELETE':
-        // Pastikan ID lokasi yang akan dihapus ada dalam URL
-        $path = explode('/', $_SERVER['REQUEST_URI']);
-        if (isset($path[3]) && is_numeric($path[3])) {
-            $idToDelete = $path[3];
-            $sql = "DELETE FROM gis WHERE id = :id";
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':id', $idToDelete, PDO::PARAM_INT);
-    
-            if ($stmt->execute()) {
-                $response = ['status' => 1, 'message' => 'Record deleted successfully.'];
-            } else {
-                $response = ['status' => 0, 'message' => 'Failed to delete record.'];
-            }
-            echo json_encode($response);
-        } else {
-            echo json_encode(['error' => 'Invalid ID for delete operation.']);
-        }
-        break;
-}
+          } else {
+            echo json_encode(["error" => "Invalid locationId"]);
+            return;
+          }
+          break;
+};
+
 ?>
